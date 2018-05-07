6d770329f483d78420230dea2b7e465cbc1a8c08
FungSaikit <675703302@qq.com>
2018-05-07 18:38:01 +0800

    5.7 新建标题栏组件

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="title">
+    <slot name="title"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TitleBar"
+  }
+</script>
+
+<style scoped>
+  .title {
+    width: 100%;
+    height: 3rem;
+    line-height: 3rem;
+    font-size: 1.2rem;
+    color: #fff;
+    background: -webkit-linear-gradient(left, #e171bd, #a678f2);
+    background: -o-linear-gradient(right, #e171bd, #a678f2);
+    background: -moz-linear-gradient(right, #e171bd, #a678f2);
+    background: linear-gradient(to right, #e171bd, #a678f2);
+    text-align: center;
+  }
+</style>
