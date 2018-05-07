e6b483091d8dfd831d69b83c382e5a13527d0a1c
FungSaikit <675703302@qq.com>
2018-05-07 18:38:28 +0800

    5.7 初始化了分类筛选的组件

---
+++ src/components/ClassFilter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <title-bar>
+        <span slot="title">筛选产品</span>
+      </title-bar>
+    </div>
+</template>
+
+<script>
+  import TitleBar from '@/components/TitleBar'
+    export default {
+        name: "ClassFilter",
+      components: {
+          TitleBar
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
