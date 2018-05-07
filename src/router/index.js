d96976ae3045c5060b96a4878a8c60c27d25d40a
FungSaikit <675703302@qq.com>
2018-05-07 18:36:06 +0800

    5.7 加入分类筛选和购物车的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Cart from '@/components/Cart'
+import ClassFilter from '@/components/ClassFilter'
 import PersonalCenter from '@/components/PersonalCenter'
+
 
 Vue.use(Router)
 
@@ -13,9 +16,19 @@ export default new Router({
       component: Index
     },
     {
+      path: '/class_filter',
+      name: 'ClassFilter',
+      component: ClassFilter
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
       path: '/personal_center',
       name: 'PersonalCenter',
       component: PersonalCenter
-    }
+    },
   ]
 })
